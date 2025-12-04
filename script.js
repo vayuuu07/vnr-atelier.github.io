ffbba8463e784b0dd58621f55a670b70b6e5c458
vayuuu07 <rajvayunandan@gmail.com>
2025-12-04 18:33:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.getElementById("contactForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  const mailBody = `Name: ${name}%0AEmail: ${email}%0A%0A${message}`;
+
+  window.location.href = `mailto:hello@vnratelier.com?subject=New Project&body=${mailBody}`;
+});
 
